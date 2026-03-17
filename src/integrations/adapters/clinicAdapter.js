dc514d0d332ee3cff7fc6bf3628c8e4133d08fec
dvera1234 <d.e.vera@hotmail.com>
2026-03-17 20:31:26 -0300

    Delete src/integrations/adapters/clinicAdapter.js

--- src/integrations/adapters/clinicAdapter.js
+++
@@ -1,9 +0,0 @@
-export function createClinicAdapter({ tenantConfig }) {
-  const provider = tenantConfig?.scheduling?.provider;
-
-  if (provider === "versatilis") {
-    return createVersatilisAdapter({ tenantConfig });
-  }
-
-  throw new Error(`Unsupported provider: ${provider}`);
-}
