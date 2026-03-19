5136abf7c1411d2a4fae0df0031967fccecddcb4
dvera1234 <d.e.vera@hotmail.com>
2026-03-18 22:01:41 -0300

    Update versatilisErrors.js

--- src/integrations/adapters/providers/versatilis/shared/versatilisErrors.js
+++ src/integrations/adapters/providers/versatilis/shared/versatilisErrors.js
@@ -1,70 +1,66 @@
-export class VersatilisError extends Error {
+export class ProviderIntegrationError extends Error {
   constructor(message, options = {}) {
     super(message);
 
-    this.name = "VersatilisError";
-
-    this.code = options.code || "VERSATILIS_ERROR";
+    this.name = "ProviderIntegrationError";
+    this.code = options.code || "PROVIDER_INTEGRATION_ERROR";
     this.httpStatus = options.httpStatus || null;
-
     this.endpoint = options.endpoint || null;
     this.rid = options.rid || null;
-
     this.isRetryable = Boolean(options.isRetryable);
-
     this.meta = options.meta || null;
   }
 }
 
-export class VersatilisAuthError extends VersatilisError {
-  constructor(message = "Versatilis authentication failed", options = {}) {
+export class ProviderAuthError extends ProviderIntegrationError {
+  constructor(message = "Provider authentication failed", options = {}) {
     super(message, {
       ...options,
-      code: "VERSATILIS_AUTH_ERROR",
+      code: "PROVIDER_AUTH_ERROR",
       httpStatus: options.httpStatus || 401,
       isRetryable: false,
     });
   }
 }
 
-export class VersatilisNetworkError extends VersatilisError {
-  constructor(message = "Versatilis network failure", options = {}) {
+export class ProviderNetworkError extends ProviderIntegrationError {
+  constructor(message = "Provider network failure", options = {}) {
     super(message, {
       ...options,
-      code: "VERSATILIS_NETWORK_ERROR",
+      code: "PROVIDER_NETWORK_ERROR",
       isRetryable: true,
     });
   }
 }
 
-export class VersatilisTimeoutError extends VersatilisError {
-  constructor(message = "Versatilis request timeout", options = {}) {
+export class ProviderTimeoutError extends ProviderIntegrationError {
+  constructor(message = "Provider request timeout", options = {}) {
     super(message, {
       ...options,
-      code: "VERSATILIS_TIMEOUT",
+      code: "PROVIDER_TIMEOUT",
       isRetryable: true,
     });
   }
 }
 
-export class VersatilisBadResponseError extends VersatilisError {
-  constructor(message = "Versatilis returned invalid response", options = {}) {
+export class ProviderBadResponseError extends ProviderIntegrationError {
+  constructor(message = "Provider returned invalid response", options = {}) {
     super(message, {
       ...options,
-      code: "VERSATILIS_BAD_RESPONSE",
+      code: "PROVIDER_BAD_RESPONSE",
       isRetryable: false,
     });
   }
 }
 
-export function normalizeVersatilisError(err, context = {}) {
-  if (err instanceof VersatilisError) {
+export function normalizeProviderError(err, context = {}) {
+  if (err instanceof ProviderIntegrationError) {
     return err;
   }
 
-  const message = err?.message || "Unknown Versatilis error";
+  const message = err?.message || "Unknown provider integration error";
 
-  return new VersatilisError(message, {
+  return new ProviderIntegrationError(message, {
     endpoint: context.endpoint,
     rid: context.rid,
     meta: {
