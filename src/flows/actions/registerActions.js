ce223fdeffb7c99ebeffe9de92568216ece95ea4
dvera1234 <d.e.vera@hotmail.com>
2026-04-03 18:01:05 -0300

    Update registerActions.js

--- src/flows/actions/registerActions.js
+++ src/flows/actions/registerActions.js
@@ -1,13 +1,19 @@
 import { registerAction } from "./actionDispatcher.js";
 
-import {
-  handlePlanMenu,
-  handlePos,
-  handleAttendant,
-} from "./menuActions.js";
+import { actionOpenSubmenu } from "./handlers/actionOpenSubmenu.js";
+import { actionGoMain } from "./handlers/actionGoMain.js";
+import { actionShowMessage } from "./handlers/actionShowMessage.js";
+import { actionPlanMenu } from "./handlers/actionPlanMenu.js";
+import { actionSelectPlan } from "./handlers/actionSelectPlan.js";
+import { actionSelectCurrentPlan } from "./handlers/actionSelectCurrentPlan.js";
+import { actionGoState } from "./handlers/actionGoState.js";
 
 export function registerDefaultActions() {
-  registerAction("PLAN_MENU", handlePlanMenu);
-  registerAction("POS", handlePos);
-  registerAction("ATTENDANT", handleAttendant);
+  registerAction("OPEN_SUBMENU", actionOpenSubmenu);
+  registerAction("GO_MAIN", actionGoMain);
+  registerAction("SHOW_MESSAGE", actionShowMessage);
+  registerAction("PLAN_MENU", actionPlanMenu);
+  registerAction("SELECT_PLAN", actionSelectPlan);
+  registerAction("SELECT_CURRENT_PLAN", actionSelectCurrentPlan);
+  registerAction("GO_STATE", actionGoState);
 }
