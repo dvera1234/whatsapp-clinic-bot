082c852ccc36bcb9f8fa56da36a1aed22e6930df
dvera1234 <d.e.vera@hotmail.com>
2026-04-20 15:41:00 -0300

    Update portalHelpers.js

--- src/flows/helpers/portalHelpers.js
+++ src/flows/helpers/portalHelpers.js
@@ -1,26 +1,26 @@
-export function getPromptByWizardState(state, MSG) {
-  switch (state) {
-    case "WZ_NOME":
-      return MSG.ASK_NOME;
-    case "WZ_DTNASC":
-      return MSG.ASK_DTNASC;
-    case "WZ_EMAIL":
-      return MSG.ASK_EMAIL;
-    case "WZ_CEP":
-      return MSG.ASK_CEP;
-    case "WZ_ENDERECO":
-      return MSG.ASK_ENDERECO;
-    case "WZ_NUMERO":
-      return MSG.ASK_NUMERO;
-    case "WZ_COMPLEMENTO":
-      return MSG.ASK_COMPLEMENTO;
-    case "WZ_BAIRRO":
-      return MSG.ASK_BAIRRO;
-    case "WZ_CIDADE":
-      return MSG.ASK_CIDADE;
-    case "WZ_UF":
-      return MSG.ASK_UF;
-    default:
-      return MSG.ASK_NOME;
+import { getWizardPromptMap } from "./contentHelpers.js";
+
+function readString(value) {
+  return typeof value === "string" ? value.trim() : "";
+}
+
+export function getPromptByWizardState(runtime, state, MSG) {
+  const promptMap = getWizardPromptMap(runtime);
+
+  const normalizedState = readString(state);
+  const messageKey =
+    readString(promptMap[normalizedState]) ||
+    readString(promptMap.default);
+
+  if (!messageKey) {
+    throw new Error("TENANT_CONTENT_MISSING:wizard.promptByState.default");
   }
+
+  const prompt = MSG?.[messageKey];
+
+  if (!readString(prompt)) {
+    throw new Error(`TENANT_CONTENT_MISSING:MSG.${messageKey}`);
+  }
+
+  return prompt;
 }
