1b292957ccbb487ae736b439d4d705e78e0cbd67
dvera1234 <d.e.vera@hotmail.com>
2026-04-21 16:00:55 -0300

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,19 @@
 import pg from "pg";
+import { DATABASE_URL } from "../config/env.js";
 
 const { Pool } = pg;
 
-export const db = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+function createPool() {
+  if (!DATABASE_URL) {
+    throw new Error("DATABASE_URL não configurado");
+  }
+
+  return new Pool({
+    connectionString: DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+}
+
+export const db = createPool();
